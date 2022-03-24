64ca3876c9d2e5c625aee93d32f638918b04e88d
Chee Kent Yong <cheekentyong@gmail.com>
2022-03-24 22:10:18 +0100

    Fixed outputs of package build results

--- build_packages.sh
+++ build_packages.sh
@@ -1,1 +1,1 @@
-DOCKER_BUILDKIT=1 docker build -f ./.github/workflows/build.Dockerfile .  --build-arg Version="$1" --target export-packages --output output 
+docker buildx build -f ./.github/workflows/build.Dockerfile . --build-arg Version="$1" --target export-packages --output type=local,dest=. 
